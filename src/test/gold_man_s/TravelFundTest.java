27fdedd8d1de4392be844b0a0734ae6b41bd0547
Yashkumar Thakur <Yashkumarthakur@gmail.com>
2021-04-05 14:32:30 +0530

    Added DecodingString Problem with TC. Refactored TravelFundTest.java file

--- src/test/gold_man_s/TravelFundTest.java
+++ src/test/gold_man_s/TravelFundTest.java
@@ -4,19 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class TravelFundTest {
 
     @Test
     void validCase() {
-        List<Integer> input = convertArrayList(new ArrayList<>(Arrays.asList(2, 3, -3)));
+        List<Integer> input = new ArrayList(Arrays.asList(2, 3, -3));
         Assertions.assertEquals(0, TravelFund.requiredAmountAtStart(input));
     }
 
     @Test
     void validCaseWithOneIn() {
-        List<Integer> input = convertArrayList(new ArrayList<>(Arrays.asList(-1)));
+        List<Integer> input = new ArrayList(Arrays.asList(-1));
         Assertions.assertEquals(2, TravelFund.requiredAmountAtStart(input));
     }
 
