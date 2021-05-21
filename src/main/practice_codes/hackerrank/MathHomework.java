8f7b76e8b8bd5b94e799280b7e598c06eead2fa6
manjusha.dhamdhere <Manjusha.Dhamdhere@nuance.com>
2021-05-21 12:39:40 +0530

    added que

--- src/main/practice_codes/hackerrank/MathHomework.java
+++ src/main/practice_codes/hackerrank/MathHomework.java
@@ -6,6 +6,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Math Homework
+* Students have been assigned a series of math problems that have points associated with them. 
+* Given a sorted points array, minimize the number of problems a student needs to solve based on the criteria below:
+* 1. They must always solve the first problem, index 0
+* 2. After solving the problem, they have a choice: solve the next problem (+1) or skip ahead and work the (+2) problem.
+* 3. Students must keep solving problems until the difference between the maximum points and the minimum points questions solved so far meets or exceeds a specified threshold
+* 4. If the student cannot meet or exceed the threshold they must work all the problems.
+* Return the minimum number of problems a student needs to solve.
+* Example:
+* Threashold = 2
+* Points = [1,2,3]
+* Output : 2
+* Explainantion:
+* Student chooses to solve problems points[0]=1 and points[2]=3
+* Diff between min and max is 3-1=2 -> which is matching threashold
+* thats why student will solve 2 problems and output is 2
+ */
 public class MathHomework {
 
 	public static void main(String[] args) {
